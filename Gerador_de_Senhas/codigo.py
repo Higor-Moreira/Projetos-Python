93374a4122d1290f2902031bc70691cb163d3b1e
Igor Oliveira <52582402+igoroliveira13@users.noreply.github.com>
2024-09-01 10:00:05 -0300

    Update codigo.py

--- Gerador_de_Senhas/codigo.py
+++ Gerador_de_Senhas/codigo.py
@@ -1,8 +1,18 @@
-# Programa gera senhas fortes 
+"""
+Programa gera senhas fortes usando listas com letras e caracteres especiais
+e escolhendo os caracteres de forma aleatorias. 
+
+A senha final contem letras minusculas, maiusculas, caracteres especiais e digitos. 
+
+"""
+from random import randint
 
 alfabeto =  ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'ç']
 caractere_especiais =  ['!', '@', '#', '$', '%', '&', '*', '?', '+', '=']
 
-senha = ''
+# fucao gera senha aleatoriamente
+def Gerar_Senha():
 
+    nova_senha = []
 
+    while nova_senha < 9:
