6c034348801bff36aecbfc4fbbcda30d32b4ab35
Igor Oliveira <52582402+igoroliveira13@users.noreply.github.com>
2024-09-01 11:24:55 -0300

    Create validador_CPF.py

---
+++ validador_CPF.py/validador_CPF.py
@@ -0,0 +1,17 @@
+"""
+Estre programa recebe um numero digitado pelo usuario 
+e verifica se o mesmo é um CPF válido. 
+"""
+
+"""def solicita_CPF():
+
+    while True:
+        entrada = input('Digite o CPF sem pontuação: ')
+    
+        try:
+            numero = int(entrada) # Tenta converter o valor digitado para int
+            return numero # se funcionar, retorno o CPF
+        except ValueError:
+            print('\033[1:31mErro! Por favor, digite somente os numeros do CPF.')"""
+
+print('\033[1:31:0mOlá mundo!\033[m')
