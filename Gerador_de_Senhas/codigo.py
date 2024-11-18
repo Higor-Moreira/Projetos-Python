71080431ec0b303a5055af60056197796564e1f2
Higor Moreira <igore.m.oliveira@gmail.com>
2024-11-18 20:13:09 -0300

    Update codigo.py

--- Gerador_de_Senhas/codigo.py
+++ Gerador_de_Senhas/codigo.py
@@ -7,12 +7,28 @@ A senha final contem letras minusculas, maiusculas, caracteres especiais e digit
 """
 from random import randint
 
-alfabeto =  ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'ç']
-caractere_especiais =  ['!', '@', '#', '$', '%', '&', '*', '?', '+', '=']
+#alfabeto =  ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'ç']
+
+#caractere_especiais =  ['!', '@', '#', '$', '%', '&', '*', '?', '+', '=']
+
+alfabeto = 'abcdefghijklmnopqrstuwvxyzç' # len = 27
+caractere_especiais = '!@#$%&*?+'        # len = 9 
 
 # fucao gera senha aleatoriamente
 def Gerar_Senha():
 
     nova_senha = []
 
-    while nova_senha < 9:
+    while len(nova_senha) < 9:
+        alfabeto_randomico = randint(1, 27)
+
+        nova_senha.append(alfabeto[alfabeto_randomico])
+
+        if len(nova_senha) == 2 or len(nova_senha) == 5:
+            especial_randomico = randint(1, 9)
+            nova_senha.append(caractere_especiais[especial_randomico])
+    
+    return nova_senha
+
+
+print(Gerar_Senha())
