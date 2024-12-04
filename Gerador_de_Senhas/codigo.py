c9cb0ea310c20df40fa1a93b9f7d65be21e388fa
Higor Moreira <igore.m.oliveira@gmail.com>
2024-12-04 17:51:09 -0300

    Update codigo.py

--- Gerador_de_Senhas/codigo.py
+++ Gerador_de_Senhas/codigo.py
@@ -2,17 +2,14 @@
 Programa gera senhas fortes usando listas com letras e caracteres especiais
 e escolhendo os caracteres de forma aleatorias. 
 
-A senha final contem letras minusculas, maiusculas, caracteres especiais e digitos. 
-
 """
 from random import randint
 
-#alfabeto =  ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'ç']
 
-#caractere_especiais =  ['!', '@', '#', '$', '%', '&', '*', '?', '+', '=']
+alfabeto = 'abcdefghijklmnopqrstuwvxyz' # len = 25
+especiais = '!@#$%&*?'
+numeros = '0123456789'
 
-alfabeto = 'abcdefghijklmnopqrstuwvxyzç' # len = 27
-caractere_especiais = '!@#$%&*?+'        # len = 9 
 
 # fucao gera senha aleatoriamente
 def Gerar_Senha():
@@ -20,15 +17,16 @@ def Gerar_Senha():
     nova_senha = []
 
     while len(nova_senha) < 9:
-        alfabeto_randomico = randint(1, 27)
+        randomico = randint(1, 25)
 
-        nova_senha.append(alfabeto[alfabeto_randomico])
+        maiscula = randint(1, 4)
+        if maiscula == 2:
+            nova_senha.append(alfabeto[randomico].upper()) 
+        else:
+            nova_senha.append(alfabeto[randomico])     
+  
+    for item in nova_senha:
+        print(item, end='')
 
-        if len(nova_senha) == 2 or len(nova_senha) == 5:
-            especial_randomico = randint(1, 9)
-            nova_senha.append(caractere_especiais[especial_randomico])
-    
-    return nova_senha
 
-
-print(Gerar_Senha())
+Gerar_Senha()
