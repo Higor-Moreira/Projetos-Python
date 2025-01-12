d5aec6458bf07c0104dd9c2dcbf0f18b77492615
Higor Moreira <igore.m.oliveira@gmail.com>
2025-01-12 18:14:22 -0300

    Update codigo.py

--- Verificador/codigo.py
+++ Verificador/codigo.py
@@ -1,12 +1,30 @@
-meu_jogo = [2, 4, 5, 6, 8, 9, 10, 11, 16, 18, 19, 21, 23, 24, 25]
+jogo_1 = [2, 4, 5, 6, 8, 9, 10, 11, 16, 18, 19, 21, 23, 24, 25]
 
-sorteio = [9, 10 , 11, 13, 14, 15, 16, 17, 18, 19, 21, 22, 23, 24, 25]
+jogo_2 = [2, 3, 5, 7, 8, 10, 11, 13, 15, 16, 18, 20, 21, 23, 24]
+
+jogo_3 = [1, 2, 5, 6, 7, 9, 11, 13, 15, 16, 18, 19, 21, 24, 25]
+
+resultado = [1, 3, 4, 5, 6, 7, 10, 12, 14, 15, 16, 17, 18, 20, 23]
 
 acertos = 0
 
-for numero in meu_jogo:
-    if numero in sorteio:
+for numero in jogo_1:
+    if numero in resultado:
         acertos += 1
 
-print(f'Voce acertou {acertos} numeros!')
+print(f'Voce acertou {acertos} numeros no jogo 1!')
+acertos = 0
 
+for numero in jogo_2:
+    if numero in resultado:
+        acertos += 1
+
+print(f'Voce acertou {acertos} numeros no jogo 2!')
+acertos = 0
+
+for numero in jogo_3:
+    if numero in resultado:
+        acertos += 1
+
+print(f'Voce acertou {acertos} numeros no jogo 3!')
+acertos = 0
