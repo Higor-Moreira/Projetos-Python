9d40d6ac0ae67d6268a3fab43842fad62b6469ed
Higor Moreira <igore.m.oliveira@gmail.com>
2026-01-10 18:26:41 -0300

    Create insertion_sort.py

---
+++ algoritmos_de_ordenacao/insertion_sort/insertion_sort.py
@@ -0,0 +1,13 @@
+'''
+O Insertion Sort funciona de forma semelhante à organização de cartas na mão:
+
+1 - Considera o primeiro elemento como já ordenado.
+2 - Pega o próximo elemento (chave).
+3 - Compara com os elementos à esquerda.
+4 - Move os maiores para a direita.
+5 - Insere a chave na posição correta.
+6 - Repete até o final da lista.
+
+'''
+
+
