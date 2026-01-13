bb6223eb5b7c47b8e71b3d58f7f4a79135220570
Higor Moreira <igore.m.oliveira@gmail.com>
2026-01-13 20:07:54 -0300

    Create blackjack.py

---
+++ jogos/blackjack/blackjack.py
@@ -0,0 +1,13 @@
+'''
+Docstring para blackjack
+
+🎯 Objetivo do jogo:
+
+Chegar o mais próximo possível de 21 pontos sem ultrapassar esse valor.
+
+* Se passar de 21 → estoura e perde.
+* Se fizer exatamente 21 → ótimo, é a melhor pontuação possível.
+
+
+
+'''
