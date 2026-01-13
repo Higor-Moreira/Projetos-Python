db073597a8ddd1c4883b7861768830c79ce28b97
Higor Moreira <igore.m.oliveira@gmail.com>
2026-01-12 21:39:16 -0300

    Create tradutor_ingles.py

---
+++ tradutor/ingles/tradutor_ingles.py
@@ -0,0 +1,12 @@
+'''
+codigo recebe uma string e busca em uma lista de palavras inglesas 
+uma correspondencia, ao localizar procura o indice da palavra em 
+uma segunda lista com palavras em portugues e realiza a troca
+'''
+
+teste = 'The quick brown fox jumps over the lazy dog'
+
+ingles = ['quick', 'brown', 'fox', 'lazy', 'dog']
+portugues = ['rapido', 'marrom', 'raposa', 'preguiçoso', 'cachorro']
+
+
